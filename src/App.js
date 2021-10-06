cb11791b7bfb591b5997518c636e424484369854
Roger <rogelio.g.sandoval@gmail.com>
2021-10-05 22:01:24 -0500

    Allow the check box to function

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import TodoList from './components/TodoList';
 
 //This is a function
 function App() {
+  //State stuff
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  //Functions
   return (
     <div className="App">
       <header>
         <h1>Roger's Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} />
       <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,7 +19,7 @@ const completeHandler = () => {
 };
     return(
         <div className="todo">
-            <li className='todo-item'>{ text }</li>
+            <li className={`todo-item ${todo.completed ? "completed" : " "}`}>{ text }</li>
             <button onClick={completeHandler} className="complete-btn"><i className="fas fa-check"></i></button>
             <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
