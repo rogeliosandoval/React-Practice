82c9b9b48e5e0ac965492c944be78b36fe84e48b
Roger <rogelio.g.sandoval@gmail.com>
2022-08-05 11:23:03 -0500

    Update the style

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,19 +16,21 @@ const Form = ( { setInputText, todos, setTodos, inputText, setStatus } ) => {
     setStatus(e.target.value);
   }
     return(
-    <form>
-      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
-      <button onClick={submitTodoHandler} className="todo-button" type="submit">
-        <i className="fas fa-plus-square"></i>
-      </button>
-      <div className="select">
-        <select onChange={statusHandler} name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
+      <div class="form-container">
+        <form>
+          <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+          <button onClick={submitTodoHandler} className="todo-button" type="submit">
+            <i className="fas fa-plus-square"></i>
+          </button>
+          <div className="select">
+            <select onChange={statusHandler} name="todos" className="filter-todo">
+              <option value="all">All</option>
+              <option value="completed">Completed</option>
+              <option value="uncompleted">Uncompleted</option>
+            </select>
+          </div>
+        </form>
       </div>
-    </form>
     );
 };
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import Todo from './Todo';
 
 const TodoList = ({ todos, setTodos, filteredTodos }) => {
     return (
-        <div className="todo-container">
+    <div className="todo-container">
       <ul className="todo-list">
           {filteredTodos.map((todo) => (
            <Todo todos={todos} setTodos={setTodos} key={todo.id} todo={todo} text={todo.text} /> 
